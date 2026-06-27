7647ca446b8995468bf7723d3acd672421c9151c
Minoru SAITO <minorusaito0819@gmail.com>
2026-06-28 00:28:57 +0900

    Remaking my portfolio with Astro

--- portfolio-src/astro.config.mjs
+++ portfolio-src/astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://minoru-s.github.io',
   base: '/portfolio/',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover',
+  },
   vite: {
     plugins: [tailwindcss()]
   }
